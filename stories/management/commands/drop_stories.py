d7494660b1e6c5029630d09603a18abbe91b8087
Chris Proctor <chris.proctor@gmail.com>
2017-08-10 09:35:47 -0400

    Removing old management commands that were shadowing new ones

--- stories/management/commands/drop_stories.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from stories.models import Story
-
-class Command(BaseCommand):
-    help = "Drop all stories"
-
-    def handle(self, *args, **options):
-        for story in Story.objects.all():
-            story.delete()
