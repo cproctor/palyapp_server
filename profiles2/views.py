312541ea07bd91caa30465a8372beb54577fec93
Chris Proctor <chris.proctor@gmail.com>
2018-04-05 15:39:35 -0400

    Adding notify route

--- profiles2/views.py
+++ profiles2/views.py
@@ -7,9 +7,11 @@ from rest_framework import permissions, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.decorators import detail_route, list_route
+from rest_framework.decorators import detail_route
 from rest_framework import mixins
 from rest_framework.permissions import AllowAny
+from django.http import HttpResponse
+from push_notifications.models import APNSDevice
 
 class ProfileViewSet(mixins.CreateModelMixin, 
                    mixins.RetrieveModelMixin, 
@@ -26,5 +28,31 @@ class ProfileViewSet(mixins.CreateModelMixin,
             'retrieve': ProfileSerializer,
             'update': ProfileSerializer,
             'partial_update': ProfileSerializer,
-            'list': UsernameProfileSerializer
+            'list': UsernameProfileSerializer,
+            'notify': ProfileSerializer
         }[self.action]
+
+    @detail_route(methods=['post', 'get'])
+    def notify(self, request, pk):
+        "Sends a test push notification to the user"
+        try:
+            device = APNSDevice.objects.get(user=self.get_object().user)
+            device.send_message(None, extra={
+                "aps":{
+                    "alert": "This is a test notification",
+                    "sound": "default",
+                    "badge": 1,
+                    "deeplink": subject.ios_deeplink()
+                }
+            })
+            return HttpResponse("Notification sent")
+
+        except APNSDevice.DoesNotExist:
+            return HttpResponse("User has not registered a device.", 
+                    status=400)
+            
+        
+
+
+
+
